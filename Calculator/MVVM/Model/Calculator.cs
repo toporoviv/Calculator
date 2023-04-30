ee391522dfdff1c07f894c494cda0a1cc2e8d814
toporoviv <ya.toporow@yandex.ru>
2023-04-30 19:09:46 +0300

    Калькулятор

---
+++ Calculator/MVVM/Model/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.MVVM.Model
+{
+    public class Calculator
+    {
+        public static readonly Dictionary<string, int> _operations = new Dictionary<string, int>()
+        {
+            { "(", 0 },
+            { ")", 0 },
+            { "*", 1 },
+            { "/", 1 },
+            { "+", 2 },
+            { "-", 2 },
+        };
+
+        public double GetAnswer(string notation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
