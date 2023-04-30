411dc1343e6783e9b417c9e62837265b5160a9e5
toporoviv <ya.toporow@yandex.ru>
2023-04-30 19:08:38 +0300

    Интерфейс для нотации

---
+++ Calculator/MVVM/Interfaces/IExpressionBuilder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.MVVM.Interfaces
+{
+    public interface IExpressionBuilder
+    {
+        List<string> GetExpression();
+
+        bool IsValid(Dictionary<string, int> operations);
+    }
+}
