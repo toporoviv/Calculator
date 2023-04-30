7639b965b0b8dddb1f7b1077ec499f597fd9555e
toporoviv <ya.toporow@yandex.ru>
2023-04-30 19:09:30 +0300

    Добавлен класс польской записи

---
+++ Calculator/MVVM/Model/PolishNotation.cs
@@ -0,0 +1,29 @@
+﻿using Calculator.MVVM.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.MVVM.Model
+{
+    public class PolishNotation : IExpressionBuilder
+    {
+        private readonly string _expression;
+
+        public PolishNotation(string expression)
+        {
+            _expression = expression;
+        }
+
+        public List<string> GetExpression()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsValid(Dictionary<string, int> operations)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
