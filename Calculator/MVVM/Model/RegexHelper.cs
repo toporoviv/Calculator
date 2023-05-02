3868794ebe9a36ebf2ef905725c192b897c7cb82
toporoviv <ya.toporow@yandex.ru>
2023-05-02 21:40:11 +0300

    Статический класс, содержащий паттерн для регулярных выражений

---
+++ Calculator/MVVM/Model/RegexHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.MVVM.Model
+{
+    public static class RegexHelper
+    {
+        public static string Pattern { get; } = @"(\d+\.\d+)|(\d+)";
+
+        static RegexHelper()
+        {
+            var builder = new RegexExpressionBuilder();
+
+            var result = builder.Build();
+
+            Pattern += $@"|([{result}])";
+        }
+    }
+}
