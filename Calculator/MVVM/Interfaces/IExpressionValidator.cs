09b3c4a44310c19b07fac51713f4243ebe372116
toporoviv <ya.toporow@yandex.ru>
2023-05-02 21:38:02 +0300

    Добавлен валидатор выражений

---
+++ Calculator/MVVM/Interfaces/IExpressionValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.MVVM.Interfaces
+{
+    public interface IExpressionValidator
+    {
+        string GetNormalizedExpression(string expression);
+
+        bool IsValidExpression(string expression);
+    }
+}
