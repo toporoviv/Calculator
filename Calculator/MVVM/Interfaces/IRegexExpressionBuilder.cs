6ed0242b755ee6eeb21a82e521675a98257f32c4
toporoviv <ya.toporow@yandex.ru>
2023-05-02 21:41:18 +0300

    Добавлены строители регулярного выражения для операторов

---
+++ Calculator/MVVM/Interfaces/IRegexExpressionBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.MVVM.Interfaces
+{
+    public interface IRegexExpressionBuilder
+    {
+        List<string> GetUsedOperations();
+
+        List<string> GetSpecialOperations();
+
+        string Build();
+    }
+}
---
+++ Calculator/MVVM/Model/RegexExpressionBuilder.cs
@@ -0,0 +1,42 @@
+﻿using Calculator.MVVM.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.MVVM.Model
+{
+    public class RegexExpressionBuilder : IRegexExpressionBuilder
+    {
+        public string Build()
+        {
+            var result = string.Empty;
+
+            var specialOperations = GetSpecialOperations();
+            var operations = GetUsedOperations();
+
+            for (int i = 0; i < operations.Count; i++)
+            {
+                if (specialOperations.Contains(operations[i])) result += $@"\{operations[i]}";
+                else result += operations[i];
+            }
+
+            return result;
+        }
+
+        public List<string> GetSpecialOperations()
+        {
+            // все символы, которые зарезервированы в регулярных выражениях
+            return new List<string>
+            {
+                "+", "-", ".", "*", "?", "^", "(", ")", "[", "]", "$", @"\", "{", "}" 
+            };
+        }
+
+        public List<string> GetUsedOperations()
+        {
+            return Calculator._operations.Keys.ToList();
+        }
+    }
+}
