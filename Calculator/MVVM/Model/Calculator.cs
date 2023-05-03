12789d5dc80f1dbfa4177784a4b4365c38f74cd7
toporoviv <ya.toporow@yandex.ru>
2023-05-03 14:19:25 +0300

    Реализован функционал калькулятора

--- Calculator/MVVM/Model/Calculator.cs
+++ Calculator/MVVM/Model/Calculator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Calculator.MVVM.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,19 +9,58 @@ namespace Calculator.MVVM.Model
 {
     public class Calculator
     {
-        public static readonly Dictionary<string, int> _operations = new Dictionary<string, int>()
+        private static readonly double _eps = 0.0001;
+
+        private static readonly Dictionary<string, Func<double, double, double>> _binaryOperations =
+            new Dictionary<string, Func<double, double, double>>
+            {
+                { "+", (x, y) => x + y },
+                { "-", (x, y) => x - y },
+                { "*", (x, y) => x * y },
+                { "/", (x, y) => Math.Abs(y) < _eps ? throw new DivideByZeroException() : x / y }
+            };
+
+        public static Dictionary<string, int> Operations { get; } = new Dictionary<string, int>()
         {
             { "(", 0 },
-            { ")", 0 },
-            { "*", 1 },
-            { "/", 1 },
+            { ")", 1 },
             { "+", 2 },
             { "-", 2 },
+            { "*", 3 },
+            { "/", 3 }
         };
 
-        public double GetAnswer(string notation)
+        public double GetAnswer(IExpressionBuilder notation)
         {
-            throw new NotImplementedException();
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+            if (notation is null) throw new ArgumentNullException();
+
+            var expression = notation.GetExpression();
+
+            int index = 0;
+            while (index < expression.Count && expression.Count != 1)
+            {
+                if (IsOperator(expression[index]))
+                {
+                    var arg1 = double.Parse(expression[index - 2]);
+                    var arg2 = double.Parse(expression[index - 1]);
+                    var result = _binaryOperations[expression[index]](arg1, arg2);
+
+                    expression.RemoveAt(index - 2);
+                    expression.RemoveAt(index - 2);
+                    expression[index - 2] = result.ToString();
+                    index = -1;
+                }
+
+                index++;
+            }
+
+            return double.Parse(expression[0]);
+        }
+
+        private bool IsOperator(string expression)
+        {
+            return Operations.ContainsKey(expression);
         }
     }
 }
