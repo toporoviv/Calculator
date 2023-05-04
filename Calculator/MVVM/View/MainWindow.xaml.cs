83966c62333b5e317095de378652234ba75b07fb
toporoviv <ya.toporow@yandex.ru>
2023-05-04 23:09:59 +0300

    -

---
+++ Calculator/MVVM/View/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Calculator.MVVM.ViewModel;
+using System.Windows;
+
+namespace Calculator.MVVM.View
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new CalculatorViewModel();
+        }
+    }
+}
