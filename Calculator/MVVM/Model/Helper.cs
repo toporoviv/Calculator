ae3d71af0316d177dab3c0c43a5c72e415ff5043
toporoviv <ya.toporow@yandex.ru>
2023-05-06 19:54:12 +0300

    Добавлен новый статический класс

---
+++ Calculator/MVVM/Model/Helper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.MVVM.Model
+{
+    public static class Helper
+    {
+        public static double GetSymbolWidth(int fontSize) => 0.65 * fontSize - 0.29;
+    }
+}
