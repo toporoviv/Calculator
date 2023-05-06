4981e34caaa5ee7e3edc0bd13de030b7734c6c83
toporoviv <ya.toporow@yandex.ru>
2023-05-06 19:55:34 +0300

    Добавлен валидатор для элемента TextBox

---
+++ Calculator/MVVM/Model/TextBoxValidator.cs
@@ -0,0 +1,38 @@
+﻿using Calculator.MVVM.Model.Validator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.MVVM.Model
+{
+    public class TextBoxValidator : BaseTextBoxValidator
+    {
+        protected override void Recalculate()
+        {
+            if (SymbolCount == 0) return;
+
+            double symbolWidth = Helper.GetSymbolWidth(FontSize);
+            double currentWidth = TextBoxWidth / (double)SymbolCount;
+            int newFontSize = FontSize;
+
+            if (currentWidth > symbolWidth)
+            {
+                if (FontSize >= MaximumWidth) return;
+
+                symbolWidth = Helper.GetSymbolWidth(++newFontSize);
+                while (newFontSize <= MaximumWidth && symbolWidth < currentWidth) symbolWidth = Helper.GetSymbolWidth(++newFontSize);
+            }
+            else
+            {
+                if (FontSize <= MinimumWidth) return;
+
+                symbolWidth = Helper.GetSymbolWidth(--newFontSize);
+                while (newFontSize >= MinimumWidth && symbolWidth > currentWidth) symbolWidth = Helper.GetSymbolWidth(--newFontSize);
+            }
+
+            FontSize = newFontSize;
+        }
+    }
+}
---
+++ Calculator/MVVM/Model/Validator/BaseTextBoxValidator.cs
@@ -0,0 +1,49 @@
+﻿using Calculator.MVVM.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.MVVM.Model.Validator
+{
+    public abstract class BaseTextBoxValidator : BaseViewModel
+    {
+        private int _fontSize;
+        private int _symbolCount;
+
+        public int FontSize
+        {
+            get => _fontSize;
+            set
+            {
+                if (value > MaximumWidth) _fontSize = MaximumWidth;
+                else _fontSize = Math.Max(MinimumWidth, value);
+
+                OnPropertyChanged(nameof(FontSize));
+            }
+        }
+
+        public int MaximumSymbolCount { get; set; } = 90;
+
+        public int TextBoxWidth { get; set; }
+
+        public int MinimumWidth { get; } = 10;
+
+        public int MaximumWidth { get; } = 20;
+
+        public int SymbolCount
+        {
+            get { return _symbolCount; }
+            set
+            {
+                if (value > MaximumSymbolCount) throw new ExpressionLengthOverflowException("Превышен лимит ввода символов");
+                _symbolCount = value;
+                Recalculate();
+            }
+        }
+
+        protected abstract void Recalculate();
+    }
+}
