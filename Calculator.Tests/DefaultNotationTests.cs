84a9e6237d1a7ef5a6554d255f14b0766fbc16f5
toporoviv <ya.toporow@yandex.ru>
2023-05-19 22:12:50 +0300

    Добавлены новые тесты

---
+++ Calculator.Tests/DefaultNotationTests.cs
@@ -0,0 +1,50 @@
+﻿using Calculator.MVVM.Model.Notation;
+using Calculator.MVVM.Model.Validator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tests
+{
+    [TestFixture]
+    public class DefaultNotationTests
+    {
+        [Test]
+        public void GetExpression_SimpleExpression_ShouldProduceCorrectDefaultNotation()
+        {
+            var expression = "1 + 2 * 3";
+
+            var notation = new DefaultNotation(new ExpressionValidator(), expression);
+
+            var result = string.Join(" ", notation.GetExpression());
+
+            Assert.That(result, Is.EqualTo("0 + " + expression));
+        }
+
+        [Test]
+        public void GetExpression_SecondSimpleExpression_ShouldProduceCorrectDefaultNotation()
+        {
+            var expression = "4 - 2 + 3 * 1.5";
+
+            var notation = new DefaultNotation(new ExpressionValidator(), expression);
+
+            var result = string.Join(" ", notation.GetExpression());
+
+            Assert.That(result, Is.EqualTo("0 + " + expression));
+        }
+
+        [Test]
+        public void GetExpression_ComplexExpression_ShouldProduceCorrectDefaultNotation()
+        {
+            var expression = "5 - 2.5 + 0.3 - 52.23 * 6 / 4 / 2.5 * 2.3";
+
+            var notation = new DefaultNotation(new ExpressionValidator(), expression);
+
+            var result = string.Join(" ", notation.GetExpression());
+
+            Assert.That(result, Is.EqualTo("0 + " + expression));
+        }
+    }
+}
---
+++ Calculator.Tests/GeneralCalculatorTests.cs
@@ -0,0 +1,56 @@
+﻿using Calculator.MVVM.Model.Notation;
+using Calculator.MVVM.Model.Validator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tests
+{
+    [TestFixture]
+    public class GeneralCalculatorTests
+    {
+        [Test]
+        public void GetAnswer_SimpleExpression_ShouldReturnCorrectResult()
+        {
+            string expression = "1 + 2 * 3";
+
+            var notation = new DefaultNotation(new ExpressionValidator(), expression);
+
+            var calculator = new Calculator.MVVM.Model.GeneralCalculator();
+
+            var result = calculator.GetAnswer(notation);
+
+            Assert.That(result, Is.EqualTo(9).Within(0.001));
+        }
+
+        [Test]
+        public void GetAnswer_SecondSimpleExpression_ShouldReturnCorrectResult()
+        {
+            string expression = "1.5 - 0.5 + 2.5 * 8 / 4";
+
+            var notation = new DefaultNotation(new ExpressionValidator(), expression);
+
+            var calculator = new Calculator.MVVM.Model.GeneralCalculator();
+
+            var result = calculator.GetAnswer(notation);
+
+            Assert.That(result, Is.EqualTo(7).Within(0.001));
+        }
+
+        [Test]
+        public void GetAnswer_ComplexExpression_ShouldReturnCorrectResult()
+        {
+            string expression = "4 / 2 - 2 + 2.5 / 0.5 - 1.5 / 5";
+
+            var notation = new DefaultNotation(new ExpressionValidator(), expression);
+
+            var calculator = new Calculator.MVVM.Model.GeneralCalculator();
+
+            var result = calculator.GetAnswer(notation);
+
+            Assert.That(result, Is.EqualTo(0.7).Within(0.001));
+        }
+    }
+}
