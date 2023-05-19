4e7a7338ec6fb03f71a1568361db2fcdfaf75d35
toporoviv <ya.toporow@yandex.ru>
2023-05-19 22:13:37 +0300

    Реализован новый тип нотации

---
+++ Calculator/MVVM/Model/Notation/DefaultNotation.cs
@@ -0,0 +1,31 @@
+﻿using Calculator.MVVM.Interfaces;
+using Calculator.MVVM.Model.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Calculator.MVVM.Model.Notation
+{
+    public class DefaultNotation : BaseExpression
+    {
+        public DefaultNotation(IExpressionValidator expressionValidator, string expression) : base(expressionValidator, expression)
+        {
+        }
+
+        public override List<string> GetExpression()
+        {
+            if (!IsValid()) throw new FormatException();
+
+            var matches = Regex.Matches(_expression, RegexHelper.Pattern);
+
+            var expression = new List<string>();
+
+            for (int i = 0; i < matches.Count; i++)
+            {
+                expression.Add(matches[i].Value);
+            }
+
+            return expression;
+        }
+    }
+}
