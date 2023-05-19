0672e3624ed5ef2c7c04ab6491a6e6f1bd12da2f
toporoviv <ya.toporow@yandex.ru>
2023-05-19 22:15:28 +0300

    Реализована фабрика для создания выражений

---
+++ Calculator/MVVM/Interfaces/IExpressionFactory.cs
@@ -0,0 +1,14 @@
+﻿using Calculator.MVVM.Model.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.MVVM.Interfaces
+{
+    public interface IExpressionFactory
+    {
+        IExpression CreateExpression(CalculatorEnum calculatorEnum, string expression);
+    }
+}
---
+++ Calculator/MVVM/Model/Factory/ExpressionFactory/ExpressionFactory.cs
@@ -0,0 +1,25 @@
+﻿using Calculator.MVVM.Interfaces;
+using Calculator.MVVM.Model.Enum;
+using Calculator.MVVM.Model.Notation;
+using Calculator.MVVM.Model.Validator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.MVVM.Model.Factory.ExpressionFactory
+{
+    public class ExpressionFactory : IExpressionFactory
+    {
+        public IExpression CreateExpression(CalculatorEnum calculatorEnum, string expression)
+        {
+            switch (calculatorEnum)
+            {
+                case CalculatorEnum.GeneralCalculator: return new DefaultNotation(new ExpressionValidator(), expression);
+                case CalculatorEnum.EngineeringCalculator: return new PolishNotation(new ExpressionValidator(), expression);
+                default: return null;
+            }
+        }
+    }
+}
